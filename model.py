0921d17f28788fa78743d71e28fc721a4899b612
CJchanghwan <94822037+CJchanghwan@users.noreply.github.com>
2024-05-27 14:33:38 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,12 +1,12 @@
+
 import math, torch, torchaudio
 import torch.nn as nn
 import torch.nn.functional as F
 import torchaudio.transforms as T
 from WavLM import WavLM, WavLMConfig
 
-checkpoint = torch.load('/your/path/WavLM-Large.pt')
+checkpoint = torch.load('/workspace/data/chgo/voxceleb_code/ECAPA-TDNN-main-blip/WavLM-Large.pt')
 cfg = WavLMConfig(checkpoint['cfg'])
-
 
 class SEModule(nn.Module):
     def __init__(self, channels, bottleneck=128):
@@ -164,10 +164,10 @@ class ECAPA_TDNN(nn.Module):
         self.pool = MHASTP(1536)
         
       
-        #self.wavlm = WavLM(cfg)
-        #self.wavlm.load_state_dict(checkpoint['model'])
-        #self.wavlm.eval() 
-        #self.layer_weights = nn.Parameter(torch.ones(25))
+        self.wavlm = WavLM(cfg)
+        self.wavlm.load_state_dict(checkpoint['model'])
+        self.wavlm.eval() 
+        self.layer_weights = nn.Parameter(torch.ones(25))
 
     def forward(self, x, aug):
 
@@ -209,6 +209,76 @@ class ECAPA_TDNN(nn.Module):
         x = x + attn
         x = F.relu(x)
         x = self.itm_head(x)
+        return x
+    
+class VANILA_ECAPA_TDNN(nn.Module):
+
+    def __init__(self, C):
+
+        super(VANILA_ECAPA_TDNN, self).__init__()
+
+        self.torchfbank = torch.nn.Sequential(
+            PreEmphasis(),            
+            torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_fft=512, win_length=400, hop_length=160, \
+                                                 f_min = 20, f_max = 7600, window_fn=torch.hamming_window, n_mels=80),
+            )
+
+        self.specaug = FbankAug() # Spec augmentation
+
+        self.conv1  = nn.Conv1d(80, C, kernel_size=5, stride=1, padding=2)
+        self.relu   = nn.ReLU()
+        self.bn1    = nn.BatchNorm1d(C)
+        self.layer1 = Bottle2neck(C, C, kernel_size=3, dilation=2, scale=8)
+        self.layer2 = Bottle2neck(C, C, kernel_size=3, dilation=3, scale=8)
+        self.layer3 = Bottle2neck(C, C, kernel_size=3, dilation=4, scale=8)
+        # I fixed the shape of the output from MFA layer, that is close to the setting from ECAPA paper.
+        self.layer4 = nn.Conv1d(3*C, 1536, kernel_size=1)
+        self.attention = nn.Sequential(
+            nn.Conv1d(4608, 256, kernel_size=1),
+            nn.ReLU(),
+            nn.BatchNorm1d(256),
+            nn.Tanh(), # I add this layer
+            nn.Conv1d(256, 1536, kernel_size=1),
+            nn.Softmax(dim=2),
+            )
+        self.bn5 = nn.BatchNorm1d(3072)
+        self.fc6 = nn.Linear(3072, 192)
+        self.bn6 = nn.BatchNorm1d(192)
+
+
+    def forward(self, x, aug):
+        with torch.no_grad():
+            x = self.torchfbank(x)+1e-6
+            x = x.log()   
+            x = x - torch.mean(x, dim=-1, keepdim=True)
+            if aug == True:
+                x = self.specaug(x)
+
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.bn1(x)
+
+        x1 = self.layer1(x)
+        x2 = self.layer2(x+x1)
+        x3 = self.layer3(x+x1+x2)
+
+        x = self.layer4(torch.cat((x1,x2,x3),dim=1))
+        x = self.relu(x)
+
+        t = x.size()[-1]
+
+        global_x = torch.cat((x,torch.mean(x,dim=2,keepdim=True).repeat(1,1,t), torch.sqrt(torch.var(x,dim=2,keepdim=True).clamp(min=1e-4)).repeat(1,1,t)), dim=1)
+
+        w = self.attention(global_x)
+
+        mu = torch.sum(x * w, dim=2)
+        sg = torch.sqrt( ( torch.sum((x**2) * w, dim=2) - mu**2 ).clamp(min=1e-4) )
+
+        x = torch.cat((mu,sg),1)
+        x = self.bn5(x)
+        x = self.fc6(x)
+        x = self.bn6(x)
+
         return x
 
 
